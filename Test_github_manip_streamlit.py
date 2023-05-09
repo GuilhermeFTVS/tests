c87eea04ba4a40786c7bab26db0b0e383f5e6622
GuilhermeFTVS <Guilherme.FONTESTAVARESDASILVA@edf-re.fr>
2023-05-09 17:17:40 +0200

    add 'files' folder

--- Test_github_manip_streamlit.py
+++ Test_github_manip_streamlit.py
@@ -6,8 +6,8 @@ from openpyxl import load_workbook
 st.button('upload file',key='upload',)
 
 if st.session_state.upload:
-    filename = '2- Loss and Uncertainty_V2.0_N117 3.6MW.xlsm'
+    filename = 'files/2- Loss and Uncertainty_V2.0_N117 3.6MW.xlsm'
     workbook = load_workbook(filename=filename)
     'deu bom!'
 
-    st.write(workbook['PastePark']['D6'].value) 
+    st.write(workbook['EYA']['E8'].value) 
